72405efa2499e067e47d94b4787ec5850de66829
Agustina Giannico <agiannico@bitsion.com>
2022-11-22 19:24:30 -0300

    componente carrousel

---
+++ components/Carrousel.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import LogoRGCP from '/public/assets/logo_blanco-RGCP.svg'
+import { useState } from 'react';
+
+export default function Footer() {
+    const [footer, setFooter] = useState(false)
+    return (
+        <h1>hola</h1>
+    )
+}
